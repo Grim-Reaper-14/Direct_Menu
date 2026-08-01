8f2b12b1f05ebad2850cb8c60b95d4c570a2b079
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-01 05:16:22 -0400

    Expand Lua binding library for sol2 API

--- src/scripting/LuaBindingLibrary.hpp
+++ src/scripting/LuaBindingLibrary.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <sol/sol.hpp>
+
+#include <functional>
+#include <string>
+#include <string_view>
+
 namespace smf::features {
 class FeatureRegistry;
 }
@@ -11,10 +17,24 @@ class LoggerApi;
 namespace smf::scripting {
 
 class LuaCommands;
+class LuaEvents;
+class LuaTimerManager;
+class LuaUI;
 
 class LuaBindingLibrary {
 public:
-    LuaBindingLibrary(logging::LoggerApi& logger, LuaCommands& commands);
+    using OwnerProvider = std::function<std::string()>;
+    using RefreshCallback = std::function<void()>;
+
+    LuaBindingLibrary(
+        logging::LoggerApi& logger,
+        sol::state& lua,
+        LuaCommands& commands,
+        LuaEvents& events,
+        LuaTimerManager& timers,
+        LuaUI& ui,
+        OwnerProvider ownerProvider,
+        RefreshCallback refreshCallback);
 
     void BindFeatureRegistry(features::FeatureRegistry& registry) noexcept;
     void RegisterCoreBindings();
@@ -22,8 +42,21 @@ public:
     [[nodiscard]] bool Ready() const noexcept;
 
 private:
+    void RegisterLogging();
+    void RegisterCommands();
+    void RegisterEvents();
+    void RegisterTimers();
+    void RegisterUI();
+    void RegisterApplication();
+
     logging::LoggerApi& logger_;
+    sol::state& lua_;
     LuaCommands& commands_;
+    LuaEvents& events_;
+    LuaTimerManager& timers_;
+    LuaUI& ui_;
+    OwnerProvider ownerProvider_;
+    RefreshCallback refreshCallback_;
     features::FeatureRegistry* registry_{nullptr};
     bool ready_{false};
 };
