f867badb060d74ac0eab4a58528696b7e4642554
The Devil Made Me Do It <156120260+Grim-Reaper-14@users.noreply.github.com>
2026-08-01 05:07:57 -0400

    Add Lua module abstraction

---
+++ src/scripting/LuaModule.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string_view>
+
+namespace smf::scripting {
+
+class LuaManager;
+
+class LuaModule {
+public:
+    virtual ~LuaModule() = default;
+
+    [[nodiscard]] virtual std::string_view Name() const noexcept = 0;
+    [[nodiscard]] virtual std::string_view Version() const noexcept = 0;
+
+    virtual bool Initialize(LuaManager& manager) = 0;
+    virtual void Update() {}
+    virtual void Shutdown() noexcept = 0;
+};
+
+} // namespace smf::scripting
